bf7b70fca64ddcd2021887819ebee18b4e736817
Vovalketer <kevinaa021@gmail.com>
2025-01-11 03:12:24 -0300

    refactor: merged command/query user services into a single one
    swapped dependencies of the affected classes

--- src/main/java/com/gray/bird/user/registration/listener/AccountVerifiedListener.java
+++ src/main/java/com/gray/bird/user/registration/listener/AccountVerifiedListener.java
@@ -5,18 +5,16 @@ import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
-import com.gray.bird.user.UserCommandService;
-import com.gray.bird.user.mapper.EventToCommandMapper;
+import com.gray.bird.user.UserService;
 import com.gray.bird.user.registration.event.AccountVerifiedEvent;
 
 @Component
 @RequiredArgsConstructor
 public class AccountVerifiedListener {
-	private final UserCommandService userCommandService;
-	private final EventToCommandMapper eventToCommandMapper;
+	private final UserService userService;
 
 	@EventListener
 	public void onAccountVerified(AccountVerifiedEvent event) {
-		userCommandService.enableAccount(eventToCommandMapper.toEnableAccountCommand(event));
+		userService.enableAccount(event.userId());
 	}
 }
--- src/test/java/com/gray/bird/user/UserServiceTest.java
+++ src/test/java/com/gray/bird/user/UserServiceTest.java
@@ -15,13 +15,12 @@ import java.util.UUID;
 import com.gray.bird.role.RoleEntity;
 import com.gray.bird.role.RoleRepository;
 import com.gray.bird.role.RoleType;
-import com.gray.bird.user.command.EnableAccountCommand;
-import com.gray.bird.user.command.RegisterUserCommand;
+import com.gray.bird.user.dto.UserCreationRequest;
 import com.gray.bird.user.event.UserEventPublisher;
 import com.gray.bird.utils.TestUtils;
 
 @SpringJUnitConfig
-public class UserCommandServiceTest {
+public class UserServiceTest {
 	@Mock
 	private RoleRepository roleRepository;
 	@Mock
@@ -34,15 +33,15 @@ public class UserCommandServiceTest {
 	private UserEventPublisher publisher;
 
 	@InjectMocks
-	private UserCommandService userCommandService;
+	private UserService userService;
 
 	private TestUtils testUtils = new TestUtils();
 
 	@Test
 	void testCreateUser() {
-		RegisterUserCommand command =
-			new RegisterUserCommand("test_user", "test@test.com", "test_handle", "test_password");
-		UserEntity user = testUtils.createUser(command.username(), command.handle(), command.email());
+		UserCreationRequest request =
+			new UserCreationRequest("test_user", "test@test.com", "test_handle", "test_password");
+		UserEntity user = testUtils.createUser(request.username(), request.handle(), request.email());
 		RoleEntity role = testUtils.createRole(RoleType.USER);
 		CredentialsEntity credentials = testUtils.createCredentials(user, "test_password");
 
@@ -50,16 +49,16 @@ public class UserCommandServiceTest {
 		Mockito.when(userRepository.save(Mockito.any(UserEntity.class))).thenReturn(user);
 		Mockito.when(credentialsRepository.save(Mockito.any(CredentialsEntity.class)))
 			.thenReturn(credentials);
-		Mockito.when(encoder.encode(command.password())).thenReturn("test_password");
+		Mockito.when(encoder.encode(request.password())).thenReturn("test_password");
 		Mockito.doNothing().when(publisher).publishUserCreatedEvent(
 			Mockito.any(UUID.class), Mockito.anyString(), Mockito.anyString(), Mockito.anyString());
 
-		userCommandService.createUser(command);
+		userService.createUser(request);
 
 		Mockito.verify(roleRepository, Mockito.times(1)).findByType(RoleType.USER);
 		Mockito.verify(userRepository, Mockito.times(1)).save(Mockito.any(UserEntity.class));
 		Mockito.verify(credentialsRepository, Mockito.times(1)).save(Mockito.any(CredentialsEntity.class));
-		Mockito.verify(encoder, Mockito.times(1)).encode(command.password());
+		Mockito.verify(encoder, Mockito.times(1)).encode(request.password());
 		Mockito.verify(publisher, Mockito.times(1))
 			.publishUserCreatedEvent(user.getUuid(), user.getUsername(), user.getHandle(), user.getEmail());
 	}
@@ -71,7 +70,7 @@ public class UserCommandServiceTest {
 		Mockito.when(userRepository.findByUuid(user.getUuid())).thenReturn(Optional.of(user));
 		Mockito.when(userRepository.save(Mockito.any(UserEntity.class))).thenReturn(user);
 
-		userCommandService.enableAccount(new EnableAccountCommand(user.getUuid()));
+		userService.enableAccount(user.getUuid());
 
 		Assertions.assertThat(user.isEnabled()).isTrue();
 		Mockito.verify(userRepository, Mockito.times(1)).findByUuid(user.getUuid());
