35f8f7a56632d74f4b0de3e7af2021990fe8fb5c
Vovalketer <kevinaa021@gmail.com>
2025-01-13 04:17:45 -0300

    test: added post creation tests, optimized imports

--- src/test/java/com/gray/bird/post/PostServiceTest.java
+++ src/test/java/com/gray/bird/post/PostServiceTest.java
@@ -1,6 +1,5 @@
 package com.gray.bird.post;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 
 import org.assertj.core.api.Assertions;
@@ -15,21 +14,89 @@ import java.util.Optional;
 
 import com.gray.bird.exception.GlobalExceptionHandler;
 import com.gray.bird.exception.ResourceNotFoundException;
-import com.gray.bird.post.PostEntity;
-import com.gray.bird.post.PostRepository;
-import com.gray.bird.post.PostService;
+import com.gray.bird.post.dto.PostCreationRequest;
+import com.gray.bird.post.dto.PostProjection;
 import com.gray.bird.utils.TestUtils;
 import com.gray.bird.utils.TestUtilsFactory;
 
 @Import(GlobalExceptionHandler.class)
 @ExtendWith(MockitoExtension.class)
-@Deprecated
 public class PostServiceTest {
 	@Mock
 	private PostRepository repository;
+	@Mock
+	private PostMapper mapper;
 	@InjectMocks
 	private PostService service;
 	private TestUtils testUtils = TestUtilsFactory.createTestUtils();
+
+	@Test
+	void createValidPostWithoutMedia() {
+		PostEntity post = testUtils.createPost();
+		PostCreationRequest req = new PostCreationRequest(post.getText(), null, post.getReplyType());
+		PostProjection projection = new PostProjection(post.getId(),
+			post.getUserId(),
+			post.getText(),
+			post.isDeleted(),
+			post.getReplyType(),
+			null,
+			post.getCreatedAt());
+
+		Mockito.when(repository.save(Mockito.any(PostEntity.class))).thenReturn(post);
+		Mockito.when(mapper.toPostProjection(Mockito.any(PostEntity.class))).thenReturn(projection);
+
+		PostProjection postResult = service.createPost(req, post.getUserId());
+
+		Assertions.assertThat(postResult).isNotNull();
+		Assertions.assertThat(postResult.id()).isEqualTo(post.getId());
+		Assertions.assertThat(postResult.text()).isEqualTo(post.getText());
+		Assertions.assertThat(postResult.userId()).isEqualTo(post.getUserId());
+		Assertions.assertThat(postResult.deleted()).isEqualTo(post.isDeleted());
+		Assertions.assertThat(postResult.replyType()).isEqualTo(post.getReplyType());
+		Assertions.assertThat(postResult.createdAt()).isEqualTo(post.getCreatedAt());
+		// ensure that the parent post is null, therefore it isnt a reply
+		Assertions.assertThat(post.getParentPost()).isNull();
+		Assertions.assertThat(post.getParentPostId()).isNull();
+		Assertions.assertThat(postResult.parentPostId()).isEqualTo(post.getParentPostId());
+
+		Mockito.verify(repository, Mockito.times(1)).save(Mockito.any(PostEntity.class));
+		Mockito.verify(mapper, Mockito.times(1)).toPostProjection(Mockito.any(PostEntity.class));
+	}
+
+	@Test
+	void createValidReplyWithoutMedia() {
+		PostEntity post = testUtils.createReply();
+		PostCreationRequest req = new PostCreationRequest(post.getText(), null, post.getReplyType());
+		PostProjection projection = new PostProjection(post.getId(),
+			post.getUserId(),
+			post.getText(),
+			post.isDeleted(),
+			post.getReplyType(),
+			post.getParentPostId(),
+			post.getCreatedAt());
+
+		Mockito.when(repository.save(Mockito.any(PostEntity.class))).thenReturn(post);
+		Mockito.when(repository.findById(Mockito.anyLong())).thenReturn(Optional.of(post.getParentPost()));
+		Mockito.when(mapper.toPostProjection(Mockito.any(PostEntity.class))).thenReturn(projection);
+
+		PostProjection postResult = service.createReply(req, post.getParentPostId(), post.getUserId());
+
+		Assertions.assertThat(postResult).isNotNull();
+		Assertions.assertThat(postResult.id()).isEqualTo(post.getId());
+		Assertions.assertThat(postResult.text()).isEqualTo(post.getText());
+		Assertions.assertThat(postResult.userId()).isEqualTo(post.getUserId());
+		Assertions.assertThat(postResult.deleted()).isEqualTo(post.isDeleted());
+		Assertions.assertThat(postResult.replyType()).isEqualTo(post.getReplyType());
+		Assertions.assertThat(postResult.createdAt()).isEqualTo(post.getCreatedAt());
+
+		// ensure that the parent post is not null, therefore it is a reply
+		Assertions.assertThat(post.getParentPost()).isNotNull();
+		Assertions.assertThat(postResult.parentPostId()).isEqualTo(post.getParentPostId());
+
+		Mockito.verify(repository, Mockito.times(1)).findById(Mockito.anyLong());
+		Mockito.verify(repository, Mockito.times(1)).save(Mockito.any(PostEntity.class));
+		Mockito.verify(mapper, Mockito.times(1)).toPostProjection(Mockito.any(PostEntity.class));
+	}
 
 	@Test
 	void getPostByPostId() {
