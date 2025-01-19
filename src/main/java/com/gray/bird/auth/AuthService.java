72062489e30b6e3a4390efb43c193560be4df47a
Vovalketer <kevinaa021@gmail.com>
2025-01-19 05:30:24 -0300

    refactor: move refresh token handling from JwtService to AuthService

--- src/main/java/com/gray/bird/auth/AuthService.java
+++ src/main/java/com/gray/bird/auth/AuthService.java
@@ -1,5 +1,7 @@
 package com.gray.bird.auth;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.Cookie.SameSite;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.CredentialsExpiredException;
 import org.springframework.security.authentication.DisabledException;
@@ -15,6 +17,8 @@ import lombok.extern.slf4j.Slf4j;
 import jakarta.servlet.http.Cookie;
 
 import java.nio.CharBuffer;
+import java.time.LocalDateTime;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -22,10 +26,10 @@ import com.gray.bird.auth.dto.LoginRequest;
 import com.gray.bird.auth.dto.LoginResponse;
 import com.gray.bird.auth.event.AuthEventPublisher;
 import com.gray.bird.auth.jwt.JwtService;
-import com.gray.bird.auth.jwt.TokenData;
 import com.gray.bird.auth.jwt.TokenType;
 import com.gray.bird.common.HttpUtils;
 import com.gray.bird.exception.ApiException;
+import com.gray.bird.exception.ExpiredJwtException;
 import com.gray.bird.exception.InvalidJwtException;
 import com.gray.bird.exception.UnauthorizedException;
 
@@ -33,22 +37,29 @@ import com.gray.bird.exception.UnauthorizedException;
 @RequiredArgsConstructor
 @Slf4j
 public class AuthService {
+	@Value("${jwt.access-token-expiration}")
+	private Integer JWT_ACCESS_TOKEN_EXPIRATION;
+	@Value("${jwt.refresh-token-expiration}")
+	private Integer JWT_REFRESH_TOKEN_EXPIRATION;
+	private String JWT_REFRESH_PATH = "/api/auth/refresh";
 	private final JwtService jwtService;
 	private final AuthCache authCache;
+	private final RefreshTokenRepository refreshTokenRepository;
 	private final BCryptPasswordEncoder encoder;
 	private final UserPrincipalService userPrincipalService;
 	private final AuthEventPublisher publisher;
 
 	public String refreshAccessToken(Cookie[] cookies) {
-		// not a fan of passing the servlet objects, taking the cookies instead
 		Optional<Cookie> cookie = HttpUtils.extractCookie(cookies, TokenType.REFRESH.getValue());
 		if (cookie.isEmpty()) {
 			throw new UnauthorizedException();
 		}
-		if (jwtService.validateRefreshToken(cookie.get().getValue())) {
-			TokenData data = jwtService.getDataFromToken(cookie.get().getValue());
-			UserPrincipal user = userPrincipalService.loadUserByUsername(data.username());
-			return jwtService.createAccessToken(user);
+		String token = cookie.get().getValue();
+
+		if (validateRefreshToken(token)) {
+			String userId = jwtService.getSubject(token);
+			UserPrincipal user = userPrincipalService.loadUserByUuid(UUID.fromString(userId));
+			return jwtService.createAccessToken(user, JWT_ACCESS_TOKEN_EXPIRATION);
 		}
 		throw new InvalidJwtException();
 	}
@@ -64,11 +75,19 @@ public class AuthService {
 			if (encoder.matches(CharBuffer.wrap(data.password()), user.getPassword())) {
 				log.info("acc/pass match, proceeding...");
 				handleLoginAttempts(user, LoginType.LOGIN_SUCCESS);
-				String accessTokenCookie = jwtService.createAccessToken(user);
-				Cookie refreshTokenCookie = jwtService.createRefreshToken(user);
+				String accessToken = jwtService.createAccessToken(user, JWT_ACCESS_TOKEN_EXPIRATION);
+				String refreshToken = jwtService.createRefreshToken(user, JWT_REFRESH_TOKEN_EXPIRATION);
+
+				Cookie refreshCookie = new Cookie(TokenType.REFRESH.getValue(), refreshToken);
+				refreshCookie.setHttpOnly(true);
+				refreshCookie.setAttribute("SameSite", SameSite.NONE.name());
+				// TODO: implement https support
+				// cookie.setSecure(true);
+				refreshCookie.setMaxAge(JWT_REFRESH_TOKEN_EXPIRATION);
+				refreshCookie.setPath(JWT_REFRESH_PATH);
 				publisher.publishUserLoggedInEvent(UUID.fromString(user.getUsername()));
 
-				return new LoginResponse(accessTokenCookie, refreshTokenCookie);
+				return new LoginResponse(accessToken, refreshCookie);
 
 			} else {
 				throw new BadCredentialsException("Incorrect email/password");
@@ -124,6 +143,21 @@ public class AuthService {
 		}
 		log.info("end of validation, success");
 		return true;
+	}
+
+	private boolean validateRefreshToken(String refreshToken) {
+		if (jwtService.validateToken(refreshToken)) {
+			RefreshTokenEntity refresh =
+				refreshTokenRepository.findByToken(refreshToken).orElseThrow(() -> new InvalidJwtException());
+			if (refresh.getExpiresAt().isBefore(LocalDateTime.now()) || refresh.getRevokedAt() != null) {
+				throw new ExpiredJwtException();
+			};
+			if (!Objects.equals(jwtService.getSubject(refreshToken), refresh.getUserId().toString())) {
+				throw new InvalidJwtException();
+			}
+			return true;
+		}
+		throw new InvalidJwtException();
 	}
 
 	public String getPrincipalUsername() {
