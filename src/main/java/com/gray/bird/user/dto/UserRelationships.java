ad0c4763cc34ff9427e064fad7c180f0afbe3bef
Vovalketer <kevinaa021@gmail.com>
2025-01-30 03:09:33 -0300

    fix: remove empty UserRelationships as there are none defined currently, set to Void

--- src/main/java/com/gray/bird/user/dto/UserRelationships.java
+++ src/main/java/com/gray/bird/user/dto/UserRelationships.java
@@ -3,6 +3,9 @@ package com.gray.bird.user.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 @AllArgsConstructor
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserRelationships {}
--- src/main/java/com/gray/bird/user/dto/UserResource.java
+++ src/main/java/com/gray/bird/user/dto/UserResource.java
@@ -2,9 +2,9 @@ package com.gray.bird.user.dto;
 
 import com.gray.bird.common.json.ResourceData;
 
-public class UserResource extends ResourceData<String, UserAttributes, UserRelationships> {
+public class UserResource extends ResourceData<String, UserAttributes, Void> {
 	public UserResource(String id, UserAttributes attributes, UserRelationships relationships) {
-		super(userType(), id, attributes, relationships);
+		super(userType(), id, attributes, null);
 	}
 
 	public static String userType() {
