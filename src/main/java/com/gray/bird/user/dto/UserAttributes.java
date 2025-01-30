57e16d158a7f092a70f5951282511cbe35efe6bc
Vovalketer <kevinaa021@gmail.com>
2025-01-30 00:27:42 -0300

    refactor: adjust the user objects to conform to the response changes

--- src/main/java/com/gray/bird/user/dto/UserAttributes.java
+++ src/main/java/com/gray/bird/user/dto/UserAttributes.java
@@ -5,7 +5,12 @@ import lombok.Builder;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonTypeName("users")
 public record UserAttributes(String username, String handle, String bio, LocalDate dateOfBirth,
 	String location, String profileImage, LocalDateTime createdAt) {
 }
---
+++ src/main/java/com/gray/bird/user/dto/UserRelationships.java
@@ -0,0 +1,8 @@
+package com.gray.bird.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class UserRelationships {}
