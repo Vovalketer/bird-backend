dfcad9643e27ff2ac6cdcf2cb7712517bfa17f80
Vovalketer <kevinaa021@gmail.com>
2025-01-29 22:31:52 -0300

    refactor: add resource paths, rename some to improve clarity

--- src/main/java/com/gray/bird/common/ResourcePaths.java
+++ src/main/java/com/gray/bird/common/ResourcePaths.java
@@ -1,12 +1,15 @@
 package com.gray.bird.common;
 
 public class ResourcePaths {
+	public static final String HOST = "http://localhost:8080";
 	public static final String BASE = "/api";
 	public static final String POSTS = BASE + "/posts";
-	public static final String LIKES = POSTS + "/{postId}/likes";
-	public static final String RETWEETS = POSTS + "/{postId}/reposts";
-	public static final String REPLIES = POSTS + "/{postId}/replies";
+	public static final String POSTS_POSTID_LIKES = POSTS + "/{postId}/likes";
+	public static final String POSTS_POSTID_REPOSTS = POSTS + "/{postId}/reposts";
+	public static final String POSTS_POSTID_REPLIES = POSTS + "/{postId}/replies";
 
 	public static final String AUTH = BASE + "/auth";
 	public static final String USERS = BASE + "/users";
+	public static final String USERS_USERNAME_POSTS = USERS + "/{username}/posts";
+	public static final String USERS_USERNAME_TIMELINES = USERS + "/{username}/timelines";
 }
