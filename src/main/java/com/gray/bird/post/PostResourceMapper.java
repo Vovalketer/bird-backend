e47fbe94d6c2f7fce4741df93cb2c88dffba59ff
Vovalketer <kevinaa021@gmail.com>
2025-01-30 00:27:06 -0300

    refactor: adjust the post objects to conform to the response changes

--- src/main/java/com/gray/bird/post/PostResourceMapper.java
+++ src/main/java/com/gray/bird/post/PostResourceMapper.java
@@ -2,51 +2,25 @@ package com.gray.bird.post;
 
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
-
-import com.gray.bird.common.jsonApi.RelationshipToOne;
-import com.gray.bird.common.jsonApi.ResourceAttributes;
-import com.gray.bird.common.jsonApi.ResourceData;
-import com.gray.bird.common.jsonApi.ResourceDataMapper;
-import com.gray.bird.common.jsonApi.ResourceFactory;
-import com.gray.bird.common.jsonApi.ResourceIdentifier;
+import com.gray.bird.common.json.ResourceMapper;
 import com.gray.bird.post.dto.PostAttributes;
 import com.gray.bird.post.dto.PostProjection;
+import com.gray.bird.post.dto.PostRelationships;
+import com.gray.bird.post.dto.PostResource;
 
 @Component
-@RequiredArgsConstructor
-public class PostResourceMapper implements ResourceDataMapper<PostProjection> {
-	private static final String PARENT = "parent";
-	private static final String USER = "user";
-	private static final String POST = "post";
-	private final ResourceFactory resourceFactory;
-
+public class PostResourceMapper implements ResourceMapper<PostProjection, PostResource> {
 	@Override
-	public ResourceData toResource(PostProjection data) {
+	public PostResource toResource(PostProjection data) {
 		PostAttributes postAttributes = getPostAttributes(data);
-		ResourceAttributes attributes = resourceFactory.createAttributes(postAttributes);
-		ResourceIdentifier identifier = resourceFactory.createIdentifier(POST, data.id().toString());
-		ResourceData resource = resourceFactory.createData(identifier, attributes);
-		if (data.parentPostId() != null) {
-			resource.addRelationshipToOne(PARENT, createParentRelationship(data));
-		}
-		resource.addRelationshipToOne(USER, createUserRelationship(data));
+		PostRelationships relationships =
+			new PostRelationships(data.userId().toString(), data.parentPostId());
+		PostResource resource = new PostResource(data.id(), postAttributes, relationships);
 
 		return resource;
 	}
 
 	private PostAttributes getPostAttributes(PostProjection data) {
 		return new PostAttributes(data.text(), data.replyType(), data.createdAt());
-	}
-
-	private RelationshipToOne createParentRelationship(PostProjection post) {
-		ResourceIdentifier identifier =
-			resourceFactory.createIdentifier(POST, post.parentPostId().toString());
-		return resourceFactory.createRelationshipToOne(identifier);
-	}
-
-	private RelationshipToOne createUserRelationship(PostProjection post) {
-		ResourceIdentifier identifier = resourceFactory.createIdentifier(USER, post.userId().toString());
-		return resourceFactory.createRelationshipToOne(identifier);
 	}
 }
