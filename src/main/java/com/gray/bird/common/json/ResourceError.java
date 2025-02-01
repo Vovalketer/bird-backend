87708dae60809d15cb9afdf72a617fabf7d1c41a
Vovalketer <kevinaa021@gmail.com>
2025-02-01 18:05:09 -0300

    refactor: comment unused parameters in the error response, add convenience method

--- src/main/java/com/gray/bird/common/json/ResourceError.java
+++ src/main/java/com/gray/bird/common/json/ResourceError.java
@@ -6,11 +6,31 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public class ResourceError {
-	private String id; // uuid
+	// private String id; // uuid
 	private String status;
-	private String code; // application error code
+	// private String code; // application error code
 	private String title;
 	private String detail;
 	private ResourceErrorSource source;
 	private Metadata metadata;
+
+	public ResourceError(String status, String title, String detail, ResourceErrorSource source) {
+		this.status = status;
+		this.title = title;
+		this.detail = detail;
+		this.source = source;
+	}
+
+	public ResourceError(String status, String title, String detail) {
+		this.status = status;
+		this.title = title;
+		this.detail = detail;
+	}
+
+	public void addMetadata(String key, Object value) {
+		if (this.metadata == null) {
+			this.metadata = new Metadata();
+		}
+		this.metadata.addMetadata(key, value);
+	}
 }
