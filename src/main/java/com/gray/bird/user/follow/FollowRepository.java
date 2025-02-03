53fc4000ef9605c860ea10f1a195bbc2fba561e1
Vovalketer <kevinaa021@gmail.com>
2025-02-03 20:37:25 -0300

    feat: add convenience method to check if a user is following another one

--- src/main/java/com/gray/bird/user/follow/FollowRepository.java
+++ src/main/java/com/gray/bird/user/follow/FollowRepository.java
@@ -1,5 +1,16 @@
 package com.gray.bird.user.follow;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface FollowRepository extends JpaRepository<FollowEntity, FollowId> {}
+import java.util.UUID;
+
+public interface FollowRepository extends JpaRepository<FollowEntity, FollowId> {
+	@Query("SELECT CASE WHEN COUNT(f)>0 THEN true ELSE false END "
+		+ "FROM FollowEntity f WHERE f.id.followingUser = :followingUser "
+		+ "AND f.id.followedUser = :followedUser")
+	Boolean
+	existsByFollowingAndFollowedId(
+		@Param("followingUser") UUID followingUser, @Param("followedUser") UUID followedUser);
+}
