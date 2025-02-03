e6ec820ca6868067d6f1da24e63a9c44a4afbfaf
Vovalketer <kevinaa021@gmail.com>
2025-02-03 16:56:44 -0300

    fix: use correct id for the timeline

--- src/main/java/com/gray/bird/timeline/TimelineRepository.java
+++ src/main/java/com/gray/bird/timeline/TimelineRepository.java
@@ -1,14 +1,14 @@
 package com.gray.bird.timeline;
 
+import java.util.UUID;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.UUID;
-
 import com.gray.bird.timeline.dto.TimelineEntryDto;
 
-interface TimelineRepository extends JpaRepository<TimelineEntity, Long> {
+interface TimelineRepository extends JpaRepository<TimelineEntity, TimelineId> {
 	Page<TimelineEntryDto> findByIdUserId(@Param("userId") UUID userId, Pageable pageable);
 }
