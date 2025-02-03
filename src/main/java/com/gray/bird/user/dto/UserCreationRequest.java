a77f837638ecf67e5a6a4d481049d784b21b025c
Vovalketer <kevinaa021@gmail.com>
2025-02-03 16:57:33 -0300

    fix: remove duplicate constraints during the user creation

--- src/main/java/com/gray/bird/user/dto/UserCreationRequest.java
+++ src/main/java/com/gray/bird/user/dto/UserCreationRequest.java
@@ -1,17 +1,19 @@
 package com.gray.bird.user.dto;
 
+import lombok.Builder;
+
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-public record UserCreationRequest(
-	@NotEmpty(message = "Username is required") @NotBlank(message = "Username is required") String username,
+@Builder
+public record UserCreationRequest(@NotBlank(message = "Username is required") String username,
 
 	@Email(message = "A valid email is required") @NotEmpty(message = "Email is required") @NotBlank(
 		message = "Email is required") String email,
 
 	@Size(min = 6, message = "Password should be at least 6 characters long") @NotEmpty(
 		message = "Password is required") @NotBlank(message = "Password is required") String password,
-	@NotEmpty(message = "Handle is required") @NotBlank(message = "Handle is required") String handle) {
+	@NotBlank(message = "Handle is required") String handle) {
 }
