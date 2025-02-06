c3a71b1bbe4abe6e14e47d64a37b1e22259f5af1
Vovalketer <kevinaa021@gmail.com>
2025-02-06 01:45:32 -0300

    test: update the test resource creator to use the new resource objects

--- src/test/java/com/gray/bird/utils/TestResources.java
+++ src/test/java/com/gray/bird/utils/TestResources.java
@@ -1,97 +1,39 @@
 package com.gray.bird.utils;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gray.bird.common.jsonApi.ResourceAttributesImpl;
-import com.gray.bird.common.jsonApi.ResourceCollectionAggregate;
-import com.gray.bird.common.jsonApi.ResourceCollectionAggregateImpl;
-import com.gray.bird.common.jsonApi.ResourceData;
-import com.gray.bird.common.jsonApi.ResourceDataImpl;
-import com.gray.bird.common.jsonApi.ResourceFactory;
-import com.gray.bird.common.jsonApi.ResourceIdentifierImpl;
-import com.gray.bird.common.jsonApi.ResourceLinksImpl;
-import com.gray.bird.common.jsonApi.ResourceMetadataImpl;
-import com.gray.bird.common.jsonApi.ResourceRelationshipsImpl;
-import com.gray.bird.common.jsonApi.ResourceResponseFactory;
-import com.gray.bird.common.jsonApi.ResourceSingleAggregate;
-import com.gray.bird.common.jsonApi.ResourceSingleAggregateImpl;
+import com.gray.bird.post.ReplyType;
+import com.gray.bird.post.dto.PostAttributes;
+import com.gray.bird.post.dto.PostRelationships;
+import com.gray.bird.post.dto.PostResource;
+import com.gray.bird.user.dto.UserAttributes;
+import com.gray.bird.user.dto.UserRelationships;
+import com.gray.bird.user.dto.UserResource;
 
 public class TestResources {
-	private ResourceResponseFactory responseFactory = new ResourceResponseFactory();
+	public PostResource createPostResource(Long postId, String userId, Long parentPostId) {
+		PostAttributes attributes = new PostAttributes(
+			UUID.randomUUID().toString(), ReplyType.EVERYONE, LocalDateTime.now().minusDays(randomInt(1000)));
+		PostRelationships reationships = new PostRelationships(userId, parentPostId);
+		return new PostResource(postId, attributes, reationships);
+	}
 
-	public ResourceData createPostResourceData() {
-		ResourceIdentifierImpl id = new ResourceIdentifierImpl("post", randomLong().toString());
-		Map<String, Object> att = Map.of("text",
+	public UserResource createUserResource(String userId) {
+		UserAttributes attributes = new UserAttributes(UUID.randomUUID().toString(),
 			UUID.randomUUID().toString(),
-			"replyType",
-			"EVERYONE",
-			"created_at",
-			LocalDateTime.now().minusSeconds(randomInt()));
-		ResourceAttributesImpl attributes = new ResourceAttributesImpl(att);
-		ResourceRelationshipsImpl relationships = new ResourceRelationshipsImpl();
-		ResourceLinksImpl links = new ResourceLinksImpl();
-		ResourceMetadataImpl meta = new ResourceMetadataImpl();
-		return new ResourceDataImpl(id, attributes, relationships, links, meta);
-	}
-
-	public List<ResourceData> createPostResourceData(int size) {
-		List<ResourceData> list = new ArrayList<>();
-		for (int i = 0; i < size; i++) {
-			list.add(createPostResourceData());
-		}
-		return list;
-	}
-
-	public ResourceSingleAggregate createPostSingleAggregate() {
-		return responseFactory.createResponse(createPostResourceData());
-	}
-
-	public ResourceCollectionAggregate createPostCollectionAggregate(int size) {
-		return responseFactory.createResponse(createPostResourceData(size));
-	}
-
-	public ResourceSingleAggregate createUserSingleAggregate() {
-		return responseFactory.createResponse(createUserResourceData());
-	}
-
-	private ResourceData createUserResourceData() {
-		ResourceIdentifierImpl id = new ResourceIdentifierImpl("user", UUID.randomUUID().toString());
-		Map<String, Object> att = Map.of("username",
 			UUID.randomUUID().toString(),
-			"handle",
+			LocalDate.now().minusDays(randomInt(100000)),
 			UUID.randomUUID().toString(),
-			"bio",
-			UUID.randomUUID().toString(),
-			"dateOfBirth",
-			LocalDateTime.now().minusSeconds(randomInt()),
-			"location",
-			UUID.randomUUID().toString(),
-			"profileImage",
-			UUID.randomUUID().toString(),
-			"createdAt",
-			LocalDateTime.now().minusSeconds(randomInt()));
-		ResourceAttributesImpl attributes = new ResourceAttributesImpl(att);
-		ResourceRelationshipsImpl relationships = new ResourceRelationshipsImpl();
-		ResourceLinksImpl links = new ResourceLinksImpl();
-		ResourceMetadataImpl meta = new ResourceMetadataImpl();
-		return new ResourceDataImpl(id, attributes, relationships, links, meta);
+			"http://www.example.com/" + UUID.randomUUID().toString() + ".jpg",
+			LocalDateTime.now().minusMinutes(randomInt(1000000)));
+		UserRelationships relationships = new UserRelationships();
+		return new UserResource(userId, attributes, relationships);
 	}
 
-	private Long randomLong() {
-		long nextLong = ThreadLocalRandom.current().nextLong();
-		Long number = nextLong;
-		return number;
-	}
-
-	private Integer randomInt() {
-		int nextInt = ThreadLocalRandom.current().nextInt();
-		Integer integer = nextInt;
-		return integer;
+	private Integer randomInt(int limit) {
+		return ThreadLocalRandom.current().nextInt(limit);
 	}
 }
