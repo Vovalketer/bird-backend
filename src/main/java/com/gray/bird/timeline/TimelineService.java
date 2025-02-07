98a6adf6cb304a2b81296550784ab5cc79321549
Vovalketer <kevinaa021@gmail.com>
2025-02-07 13:56:20 -0300

    feat: implement Following feed

--- src/main/java/com/gray/bird/timeline/TimelineService.java
+++ src/main/java/com/gray/bird/timeline/TimelineService.java
@@ -7,15 +7,18 @@ import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.UUID;
 
 import com.gray.bird.timeline.dto.TimelineEntryDto;
+import com.gray.bird.user.follow.FollowService;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TimelineService {
 	private final TimelineRepository repository;
+	private final FollowService followService;
 
 	public Page<TimelineEntryDto> getHomeTimeline(UUID userId, Pageable pageable) {
 		return repository.findByUserId(userId, pageable);
@@ -29,5 +32,10 @@ public class TimelineService {
 	@Transactional
 	public void deleteEntry(UUID userId, Long postId) {
 		repository.delete(new TimelineEntity(userId, postId));
+	}
+
+	public Page<TimelineEntryDto> getFollowingTimeline(UUID userId, Pageable pageable) {
+		List<UUID> following = followService.getFollowing(userId);
+		return repository.findByUserIdsIn(following, pageable);
 	}
 }
--- src/test/java/com/gray/bird/timeline/TimelineControllerIT.java
+++ src/test/java/com/gray/bird/timeline/TimelineControllerIT.java
@@ -4,16 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.TestingAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.util.UUID;
+
+import com.gray.bird.auth.jwt.JwtService;
 import com.gray.bird.common.ResourcePaths;
 import com.gray.bird.testConfig.TestcontainersConfig;
 
@@ -24,13 +33,49 @@ import com.gray.bird.testConfig.TestcontainersConfig;
 @Sql(scripts = {"/sql/mockaroo/roles.sql",
 		 "/sql/mockaroo/users.sql",
 		 "/sql/mockaroo/posts.sql",
-		 "/sql/mockaroo/timelines.sql"})
+		 "/sql/mockaroo/timelines.sql",
+		 "/sql/mockaroo/follows.sql"})
 public class TimelineControllerIT {
 	@Autowired
 	private MockMvc mockMvc;
+	@MockitoBean
+	private JwtService jwtService;
 
 	private String baseUrl = ResourcePaths.FEEDS;
 
 	@Nested
-	class UnauthenticatedUser {}
+	class AuthenticatedUser {
+		// our trusty friend "mtompion1"
+		// private UUID USER_ID = UUID.fromString("4e27b52d-b86c-448e-9e9e-db1d38c9a55c");
+		private UUID USER_ID = UUID.fromString("f7caceb3-a9dd-4cf2-8fde-7576bb7d0b2d");
+		private String ACCESS_TOKEN = "Bearer accessToken";
+
+		@BeforeEach
+		void setUp() {
+			Authentication authentication = new TestingAuthenticationToken(USER_ID, "password", "USER");
+			Mockito.when(jwtService.validateToken(Mockito.anyString())).thenReturn(true);
+			Mockito.when(jwtService.getAuthenticationFromAccessToken(Mockito.anyString()))
+				.thenReturn(authentication);
+		}
+
+		@Test
+		void shouldFollowingFeedWhenRequested() throws Exception {
+			mockMvc
+				.perform(MockMvcRequestBuilders.get(baseUrl + "/following")
+						.header(HttpHeaders.AUTHORIZATION, ACCESS_TOKEN))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.data.length()", Matchers.greaterThan(0)))
+				.andExpect(
+					MockMvcResultMatchers.jsonPath("$.included.users.length()", Matchers.greaterThan(0)));
+		}
+	}
+
+	@Nested
+	class UnauthenticatedUser {
+		@Test
+		void shouldReturnUnauthorizedWhenFeedIsRequested() throws Exception {
+			mockMvc.perform(MockMvcRequestBuilders.get(baseUrl + "/following"))
+				.andExpect(MockMvcResultMatchers.status().isUnauthorized());
+		}
+	}
 }
