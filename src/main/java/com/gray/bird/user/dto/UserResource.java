f66c4bb3a3fbd094732f9ffea3c47316f316ee16
Vovalketer <kevinaa021@gmail.com>
2025-02-18 01:09:07 -0300

    feat: posts and users set their own self links upon instantiation

--- src/main/java/com/gray/bird/user/dto/UserResource.java
+++ src/main/java/com/gray/bird/user/dto/UserResource.java
@@ -1,11 +1,13 @@
 package com.gray.bird.user.dto;
 
+import com.gray.bird.common.ResourcePaths;
 import com.gray.bird.common.ResourceType;
 import com.gray.bird.common.json.ResourceData;
 
 public class UserResource extends ResourceData<String, UserAttributes, Void> {
 	public UserResource(String id, UserAttributes attributes, UserRelationships relationships) {
 		super(userType(), id, attributes, null);
+		super.getLinks().setSelf(ResourcePaths.USERS + "/" + attributes.username());
 	}
 
 	public static String userType() {
