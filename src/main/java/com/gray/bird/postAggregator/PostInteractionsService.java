09730f981d2bc108db3dac2211516efd31d9d592
Vovalketer <kevinaa021@gmail.com>
2025-02-26 00:40:45 -0300

    feat: add summary functions to provide more data related to the interactions and expand PostInteractions

--- src/main/java/com/gray/bird/postAggregator/PostInteractionsService.java
+++ src/main/java/com/gray/bird/postAggregator/PostInteractionsService.java
@@ -7,14 +7,17 @@ import lombok.RequiredArgsConstructor;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import com.gray.bird.like.LikeService;
+import com.gray.bird.like.dto.LikeSummary;
 import com.gray.bird.like.dto.LikesCount;
 import com.gray.bird.post.PostService;
 import com.gray.bird.post.dto.RepliesCount;
 import com.gray.bird.postAggregator.dto.PostInteractions;
 import com.gray.bird.repost.RepostService;
+import com.gray.bird.repost.dto.RepostSummary;
 import com.gray.bird.repost.dto.RepostsCount;
 
 @Service
@@ -25,8 +28,8 @@ public class PostInteractionsService {
 	private final PostService postService;
 
 	public PostInteractions getInteractionsById(Long id) {
-		Long likesCount = likesService.getLikesCountByPostId(id).likesCount();
-		Long repostCount = repostService.getRepostCountByPostId(id).repostsCount();
+		Long likesCount = likesService.getLikeSummary(id).likesCount();
+		Long repostCount = repostService.getRepostSummary(id).repostsCount();
 		Long repliesCount = postService.getRepliesCountByPostId(id).repliesCount();
 
 		return new PostInteractions(id, repliesCount, likesCount, repostCount);
@@ -54,6 +57,46 @@ public class PostInteractionsService {
 													 repostCountsMap.getOrDefault(postId, 0L)))
 											 .collect(Collectors.toList());
 
+		return collect;
+	}
+
+	public PostInteractions getInteractionsById(Long id, UUID userId) {
+		RepliesCount repliesCount = postService.getRepliesCountByPostId(id);
+		LikeSummary likeSummary = likesService.getLikeSummary(userId, id);
+		RepostSummary repostSummary = repostService.getRepostSummary(userId, id);
+		System.out.println("LikeSummary: " + likeSummary);
+		return new PostInteractions(id,
+			repliesCount.repliesCount(),
+			likeSummary.likesCount(),
+			repostSummary.repostsCount(),
+			likeSummary.isLiked(),
+			repostSummary.isReposted(),
+			likeSummary.createdAt(),
+			repostSummary.createdAt());
+	}
+
+	public List<PostInteractions> getAllInteractionsByIds(Collection<Long> postIds, UUID userId) {
+		List<LikeSummary> likes = likesService.getLikeSummaryByPostIds(userId, postIds);
+		List<RepostSummary> reposts = repostService.getRepostSummaryByPostIds(userId, postIds);
+		List<RepliesCount> replies = postService.getRepliesCountByPostIds(postIds);
+
+		Map<Long, LikeSummary> likesMap = likes.stream().collect(Collectors.toMap(l -> l.postId(), l -> l));
+		Map<Long, RepostSummary> repostsMap =
+			reposts.stream().collect(Collectors.toMap(r -> r.postId(), r -> r));
+		Map<Long, Long> repliesMap =
+			replies.stream().collect(Collectors.toMap(r -> r.postId(), r -> r.repliesCount()));
+
+		List<PostInteractions> collect = postIds.stream()
+											 .map(postId
+												 -> new PostInteractions(postId,
+													 repliesMap.get(postId),
+													 likesMap.get(postId).likesCount(),
+													 repostsMap.get(postId).repostsCount(),
+													 likesMap.get(postId).isLiked(),
+													 repostsMap.get(postId).isReposted(),
+													 likesMap.get(postId).createdAt(),
+													 repostsMap.get(postId).createdAt()))
+											 .collect(Collectors.toList());
 		return collect;
 	}
 }
