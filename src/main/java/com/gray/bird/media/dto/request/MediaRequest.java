e3899fd1bf1ee17c0c65e1bec2d50ba734b9a7d4
Vovalketer <kevinaa021@gmail.com>
2025-04-20 14:02:39 -0300

    feat: prevent MediaRequest content from being null and validate the amount of metadata

--- src/main/java/com/gray/bird/media/dto/request/MediaRequest.java
+++ src/main/java/com/gray/bird/media/dto/request/MediaRequest.java
@@ -2,17 +2,31 @@ package com.gray.bird.media.dto.request;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 public record MediaRequest(List<MultipartFile> files, Map<Integer, MediaMetadataRequest> metadata) {
+	public MediaRequest(List<MultipartFile> files, Map<Integer, MediaMetadataRequest> metadata) {
+		if (files == null) {
+			files = List.of();
+		}
+		if (metadata == null) {
+			metadata = Collections.emptyMap();
+		}
+		if ((metadata.size() > files.size())) {
+			throw new IllegalArgumentException("Metadata size does not match files size");
+		}
+
+		// this is to appease the IDE, the constructor shouldnt need this
+		this.files = files;
+		this.metadata = metadata;
+	}
 	public MediaRequest(List<MultipartFile> files) {
-		this(files, new HashMap<>());
+		this(files, Collections.emptyMap());
 	}
 
 	public MediaRequest() {
-		this(new ArrayList<>());
+		this(List.of(), Collections.emptyMap());
 	}
 }
