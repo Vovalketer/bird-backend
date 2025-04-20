316f3b947ca22b178e296866afccd59b89fe1895
Vovalketer <kevinaa021@gmail.com>
2025-04-20 01:02:24 -0300

    chore: remove unused mapper methods

--- src/main/java/com/gray/bird/media/MediaMapper.java
+++ src/main/java/com/gray/bird/media/MediaMapper.java
@@ -2,27 +2,15 @@ package com.gray.bird.media;
 
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 import java.util.List;
 import java.util.Set;
 
 import com.gray.bird.media.dto.MediaDto;
-import com.gray.bird.media.dto.MediaProjection;
-import com.gray.bird.media.dto.MediaRequest;
-import com.gray.bird.media.dto.MediaRequestContent;
 
 @Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface MediaMapper {
 	MediaDto toMediaDto(MediaEntity media);
 
 	List<MediaDto> toMediaDto(Set<MediaEntity> media);
-
-	MediaProjection toMediaProjection(MediaEntity media);
-
-	List<MediaProjection> toMediaProjection(List<MediaEntity> media);
-
-	MediaEntity toMediaEntity(MediaRequest media);
-
-	MediaEntity toMediaEntity(MediaRequestContent media);
 }
--- src/main/java/com/gray/bird/post/PostMapper.java
+++ src/main/java/com/gray/bird/post/PostMapper.java
@@ -4,12 +4,9 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 import com.gray.bird.media.MediaMapper;
-import com.gray.bird.post.dto.PostCreationRequest;
 import com.gray.bird.post.dto.PostProjection;
 
 @Mapper(uses = MediaMapper.class, injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PostMapper {
-	PostEntity toPostEntity(PostCreationRequest post);
-
 	PostProjection toPostProjection(PostEntity post);
 }
